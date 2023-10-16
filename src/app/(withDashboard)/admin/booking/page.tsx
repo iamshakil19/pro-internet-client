58fdca2d7e9d3ae07db4a821ba90daf1c25d5ca7
iamshakil19 <shakilahmed.pure@gmail.com>
2023-10-16 15:10:25 +0600

    some new page added

---
+++ src/app/(withDashboard)/admin/booking/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BookingPage = () => {
+  return (
+    <div>
+      <h3>This is booking page</h3>
+    </div>
+  );
+};
+
+export default BookingPage;
---
+++ src/app/(withDashboard)/admin/faq/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FaqPage = () => {
+  return <div>This is faq page</div>;
+};
+
+export default FaqPage;
