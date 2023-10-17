288c884569fa21c3db5d66f99f6552ae20d449b9
iamshakil19 <shakilahmed.pure@gmail.com>
2023-10-17 13:03:57 +0600

    card dynamic

--- src/app/(public)/package/page.tsx
+++ src/app/(public)/package/page.tsx
@@ -1,11 +1,29 @@
+"use client";
 
 import PackageCard from "@/components/Package/PackageCard";
+import { useGetAllPackageQuery } from "@/redux/api/packageApi";
+import { IPackage } from "@/types";
 import React from "react";
 
 const PackagePage = () => {
+  const { data, isLoading } = useGetAllPackageQuery({});
+  console.log(data?.package);
+
   return (
-    <div className="container mx-auto p-5">
-      <PackageCard />
+    <div className="container p-5 mx-auto">
+      <div className="mt-10 pb-10 border-b">
+        <span className="font-semibold text-blue-500">PRICING PLANS</span>
+        <h3 className="text-4xl font-bold">Choose the best plan</h3>
+        <p className="text-lg font-medium mt-2">
+          Honest and affordable pricing model to help you get started easily.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mt-10 mb-10">
+        {data?.package?.map((pack: any) => (
+          <PackageCard pack={pack} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Package/PackageCard.tsx
+++ src/components/Package/PackageCard.tsx
@@ -3,18 +3,37 @@
 import React from "react";
 import { CheckCircleOutlined } from "@ant-design/icons";
 
-const PackageCard = () => {
+const PackageCard = ({ pack }: any) => {
+  const {
+    id,
+    name,
+    price,
+    renewsFee,
+    category,
+    storage,
+    bandwidth,
+    website,
+    cpu,
+    physicalMemory,
+    process,
+    desc,
+    createdAt,
+    updatedAt,
+  } = pack || {};
   return (
-    <div className="w-full max-w-xs shadow-lg rounded-md">
+    <div className="w-full max-w-xs shadow-lg rounded-md mx-auto">
       <div className="bg-[#E6F1FF] p-5 rounded-md">
-        <p className="bg-gray-400 text-white font-semibold text-lg text-center px-4 py-0.5 rounded-3xl w-14 mx-auto">
-          Go
+        <p className="text-center">
+          <span className="bg-gray-400 text-white font-semibold text-lg text-center px-4 py-0.5 rounded-3xl mx-auto">
+            {name}
+          </span>
         </p>
         <p className="text-center mt-3 font-medium">
-          <span className="text-3xl font-bold">TK 120</span> /month
+          <span className="text-3xl font-bold">TK {price}</span> /month
         </p>
-        <p className="text-center mt-1 font-medium text-sm">
-          Perfect for small websites
+        <p className="text-center mt-1 font-medium text-sm">{desc}</p>
+        <p className="text-center border border-gray-300 rounded-full mt-2 py-0.5 text-sm">
+          Renews at TK {renewsFee} /mo
         </p>
       </div>
 
@@ -22,43 +41,37 @@ const PackageCard = () => {
         <div className="flex text-center gap-2">
           <CheckCircleOutlined style={{ color: "green" }} />
           <span>
-            <strong>1 GB </strong>SSD Storage
+            <strong>{storage} GB </strong>SSD Storage
           </span>
         </div>
         <div className="flex text-center gap-2 mt-2">
           <CheckCircleOutlined style={{ color: "green" }} />
           <span>
-            <strong>1 GB </strong>SSD Storage
+            <strong>{bandwidth} </strong>Bandwidth
           </span>
         </div>
         <div className="flex text-center gap-2 mt-2">
           <CheckCircleOutlined style={{ color: "green" }} />
           <span>
-            <strong>1 GB </strong>SSD Storage
+            <strong>{website}</strong>Website
           </span>
         </div>
         <div className="flex text-center gap-2 mt-2">
           <CheckCircleOutlined style={{ color: "green" }} />
           <span>
-            <strong>1 GB </strong>SSD Storage
+            <strong>{cpu} Core </strong>CPU
           </span>
         </div>
         <div className="flex text-center gap-2 mt-2">
           <CheckCircleOutlined style={{ color: "green" }} />
           <span>
-            <strong>1 GB </strong>SSD Storage
+            <strong>{physicalMemory} GB </strong>Physical Memory
           </span>
         </div>
         <div className="flex text-center gap-2 mt-2">
           <CheckCircleOutlined style={{ color: "green" }} />
           <span>
-            <strong>1 GB </strong>SSD Storage
-          </span>
-        </div>
-        <div className="flex text-center gap-2 mt-2">
-          <CheckCircleOutlined style={{ color: "green" }} />
-          <span>
-            <strong>1 GB </strong>SSD Storage
+            <strong>{process} GB </strong>Entry Process
           </span>
         </div>
       </div>
