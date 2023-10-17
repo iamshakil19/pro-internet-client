9038e5c4d3b933d03bd57217cb4cf2422aa42df8
iamshakil19 <shakilahmed.pure@gmail.com>
2023-10-18 05:57:18 +0600

    feedback user list done

--- src/components/Feedback/FeedbackList.tsx
+++ src/components/Feedback/FeedbackList.tsx
@@ -5,9 +5,19 @@ import { getUserInfo } from "@/services/auth.service";
 import React from "react";
 
 const FeedbackList = ({ email }: any) => {
+  const { data } = useGetAllFeedbackQuery({});
 
-  const { data } = useGetAllFeedbackQuery({ email: email });
-  return <div>this is feedback list</div>;
+  const { feedback } = data || {};
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+      {feedback?.map((item: any) => (
+        <div className="w-full bg-white p-3 rounded-lg shadow-lg max-w-xs mx-0">
+          {item.desc}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default FeedbackList;
